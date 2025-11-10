935049759182e90828552ed2799219e6ca4f96a9
cnm5084 <cnm5084@gmail.com>
2025-11-10 19:51:22 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_midterm
+
+A new Flutter project.
---
+++ lib/myplaces.dart
@@ -0,0 +1,250 @@
+// ignore_for_file: avoid_print
+
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class Myplaces extends StatefulWidget {
+  const Myplaces({super.key});
+
+  @override
+  State<Myplaces> createState() => _MyplacesState();
+}
+
+class _MyplacesState extends State<Myplaces> {
+  List<dynamic> placeList = [];
+  String status = "Press the button to get place.";
+  bool isLoading = false;
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    if (screenWidth > 600) {
+      screenWidth = 600; //limit max width for better readability
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Place', style: TextStyle(fontWeight: FontWeight.bold)),
+        backgroundColor: const Color.fromARGB(255, 136, 156, 113),
+      ),
+      body: Center(
+        child: isLoading
+            ? Column(
+                children: [
+                  SizedBox(height: 100, child: Center(child: Text(status))),
+                  CircularProgressIndicator(),
+                ],
+              )
+            : Column(
+                children: [
+                  SizedBox(height: 10),
+                  Text(
+                    'Let\'s explore Malaysia!',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  SizedBox(height: 10),
+                  SizedBox(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        getPlace();
+                      },
+                      child: Text('Explore random place.'),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  placeList.isEmpty
+                      ? SizedBox(
+                          height: 100,
+                          child: Center(child: Text(status)),
+                        )
+                      : Expanded(
+                          child: ListView.builder(
+                            itemCount: placeList.length,
+                            itemBuilder: (context, index) {
+                              String placeName = placeList[index]['name'];
+                              String state = placeList[index]['state'];
+                              String imageUrl = placeList[index]['image_url'];
+                              double rating = (placeList[index]['rating'] ?? 0)
+                                  .toDouble();
+
+                              return SizedBox(
+                                height: 220,
+                                child: Card(
+                                  elevation: 2,
+                                  shadowColor: const Color.fromARGB(255,103,102,133,),
+                                  margin: EdgeInsets.symmetric(
+                                    vertical: 8,
+                                    horizontal: 8,
+                                  ),
+                                  child: Row(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      SizedBox(width: 15),
+                                      Center(
+                                        child: Container(
+                                          width: 150,
+                                          height: 180,
+                                          decoration: BoxDecoration(
+                                            border: Border.all(
+                                              color: const Color.fromARGB(255,27,41,28,),
+                                            ),
+                                          ),
+                                          child: Image.network(
+                                            imageUrl,
+                                            width: 150,
+                                            height: 150,
+                                            fit: BoxFit.cover,
+                                            errorBuilder:
+                                                (context, error, stackTrace) =>
+                                                    const Icon(
+                                                      Icons.error,
+                                                      color: Colors.red,
+                                                    ),
+                                          ),
+                                        ),
+                                      ),
+                                      SizedBox(width: 20),
+                                      Expanded(
+                                        child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            SizedBox(height: 20),
+                                            Text(
+                                              placeName,
+                                              style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 20,
+                                              ),
+                                            ),
+                                            SizedBox(height: 15),
+                                            Text('State: $state',
+                                            style: TextStyle(fontSize: 16),),
+                                            Text('Rating: $rating',
+                                            style: TextStyle(fontSize: 16),),
+                                          ],
+                                        ),
+                                      ),
+                                      Center(
+                                        child: IconButton(
+                                          icon: Icon(Icons.arrow_forward_ios),
+                                          onPressed: () {
+                                            showDialog(
+                                              context: context,
+                                              builder: (context) {
+                                                String description = placeList[index]['description'];
+                                                String contact =
+                                                    placeList[index]['contact'];
+                                                double latitude = double.tryParse(placeList[index]['latitude'].toString(), ) ??0.0;
+                                                double longitude = double.tryParse(placeList[index]['longitude'].toString(),) ??0.0;
+                                                String category = placeList[index]['category'];
+
+                                                return AlertDialog(
+                                                  title: Text(placeName,
+                                                  style: TextStyle(
+                                                    fontWeight: FontWeight.bold
+                                                  ),),
+                                                  content: SizedBox(
+                                                    width: double.maxFinite,
+                                                    child: SingleChildScrollView(
+                                                      child: Column(
+                                                        crossAxisAlignment:CrossAxisAlignment.start,
+                                                        children: [
+                                                          Center(
+                                                            child: Container(
+                                                              width: 250,
+                                                              height: 280,
+                                                              decoration: BoxDecoration(
+                                                                border: Border.all(
+                                                                  color:
+                                                                      const Color.fromARGB( 255,27,41,28,),
+                                                                ),
+                                                              ),
+                                                              child: Image.network(
+                                                                imageUrl,
+                                                                width: 250,
+                                                                height: 280,
+                                                                fit: BoxFit.cover,
+                                                                errorBuilder:
+                                                                    (context,error,stackTrace) => const Icon(
+                                                                      Icons.error,color: Colors.red,
+                                                                    ),
+                                                              ),
+                                                            ),
+                                                          ),
+                                                          SizedBox(height: 10),
+                                                          Text('Description: ',style: TextStyle(fontWeight:FontWeight.bold),),
+                                                          Text(description),
+                                                          SizedBox(height: 5),
+                                                          Text('Contact: ',style: TextStyle(fontWeight:FontWeight.bold,),),
+                                                          Text(contact),
+                                                          SizedBox(height: 5,),
+                                                          Text('Location: ',
+                                                            style: TextStyle(fontWeight:FontWeight.bold),),
+                                                          Text('Latitude: $latitude'),
+                                                          Text('Longitude: $longitude '),
+                                                          SizedBox(height: 5,),
+                                                          Text('Category: ',style: TextStyle(fontWeight:FontWeight.bold),),
+                                                          Text(category),
+                                                        ],
+                                                      ),
+                                                    ),
+                                                  ),
+                                                  actions: [
+                                                    TextButton(
+                                                      onPressed: () =>
+                                                          Navigator.of(context,).pop(),
+                                                      child: const Text(
+                                                        'Close',
+                                                      ),
+                                                    ),
+                                                  ],
+                                                );
+                                              },
+                                            );
+                                          },
+                                        ),
+                                      ),
+                                      SizedBox(width: 10),
+                                    ],
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                ],
+              ),
+      ),
+    );
+  }
+
+  Future<void> getPlace() async {
+    status = "Fetching places...";
+    isLoading = true;
+    setState(() {});
+
+    try {
+      final response = await http.get(
+        Uri.parse(
+          'https://slumberjer.com/teaching/a251/locations.php?state=&category=&name=',
+        ),
+      );
+
+      if (response.statusCode == 200) {
+        var data = response.body;
+        placeList = json.decode(data);
+        status = "Places loaded.";
+      } else {
+        print('Failed to load places');
+        status = "Failed to load places. Please try again later.";
+      }
+    } catch (error) {
+      print('Error: $error');
+      status = "Connection error. Please check your internet.";
+    } finally {
+      isLoading = false;
+      setState(() {});
+    }
+  }
+}
